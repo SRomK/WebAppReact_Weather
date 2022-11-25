0918e54c4dfbbe2cf1a4725cfc39843a602779a8
Sofia <75990610+SRomK@users.noreply.github.com>
2022-11-25 18:51:39 +0100

    current-weather component done

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from '../../api';
+import { geoApiOptions, GEO_API_URL } from '../../api';
 
 const Search = ({ onSearchChange }) => {
 
@@ -8,27 +8,26 @@ const Search = ({ onSearchChange }) => {
 
     const loadOptions = (inputValue) => {
         return fetch(
-            `${GEO_API_URL}/cities??minPopuopulation=1000000&namePrefix=${inputValue}`,
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
             geoApiOptions
         )
-            .then(response => response.json())
-            .then(response => {
+            .then((response) => response.json())
+            .then((response) => {
                 return {
                     options: response.data.map((city) => {
                         return {
-                            value: `${city.latitude}, ${city.longitude}`,
+                            value: `${city.latitude} ${city.longitude}`,
                             label: `${city.name}, ${city.countryCode}`,
-                        }
-                    })
-                }
-            })
-            .catch(err => console.error(err));
-    }
+                        };
+                    }),
+                };
+            });
+    };
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
-    }
+    };
 
     return (
         <AsyncPaginate
@@ -38,8 +37,8 @@ const Search = ({ onSearchChange }) => {
             onChange={handleOnChange}
             loadOptions={loadOptions}
         />
-    )
-}
+    );
+};
 
 
 export default Search;
