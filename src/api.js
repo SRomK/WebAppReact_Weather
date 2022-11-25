3c92cec35eeb4665cfe3c49e759cf16057f62125
Sofia <75990610+SRomK@users.noreply.github.com>
2022-11-25 01:58:14 +0100

    search component ready

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '972d5cc10cmsh6257be5d227fca5p19b12bjsn97316dab4c93',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
