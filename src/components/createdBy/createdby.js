993f286cba858ae27431d6feed12cada7dad56e3
Sofia <75990610+SRomK@users.noreply.github.com>
2022-11-28 13:25:31 +0100

    router dom package i and createdby component

---
+++ src/components/createdBy/createdby.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './createdby.css';
+
+
+const CreatedBy = () => {
+    return (
+        <div className="createdby-container">
+
+            <h3 className="createdby-h3">Created By:</h3>
+            
+                <a
+                    className="createdby-a"
+                    href="https://www.linkedin.com/in/sofiaromerokamermann/"
+                    target="_blank" rel="noreferrer"
+                >
+                    Sofia Romero Kamermann
+                </a>
+            
+        </div>
+    );
+}
+
+export default CreatedBy;
