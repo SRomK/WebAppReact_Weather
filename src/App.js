97bef1c327979886c2e9d12d53910027ab90f328
Sofia <75990610+SRomK@users.noreply.github.com>
2022-11-29 16:06:34 +0100

    modified favicon, comments to rmv

--- src/App.js
+++ src/App.js
@@ -12,34 +12,36 @@ function App() {
   const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
+    const [lat, lon] = searchData.value.split(" "); /* destructuracion en forma de arrays de donde salen 2 posicion, uno y dos */
 
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
+    ); /* le paso 4 parametros al fetch---> wapiurl, lat, lon y weapikey */
 
     const forecastFetch = fetch(
       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
 
-    Promise.all([currentWeatherFetch, forecastFetch]) 
+    Promise.all([currentWeatherFetch, forecastFetch]) /* dentro del promise all siempre tendra que ir un array formado por las promesas/fetches*/
       .then(async (response) => {
-        const weatherResponse = await response[0].json();
+        const weatherResponse = await response[0].json(); /* viene de json y lo convierte a un formato legible para js */
         const forecastResponse = await response[1].json();
 
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setCurrentWeather({ city: searchData.label, ...weatherResponse }); /*  los 3 puntos es estar guardando los valres de weather response// el spread operator hace que me quede con los valores dle objeto, no con el objeto en si*/
+        console.log("con puntos", { city: searchData.label, ...weatherResponse });
+        console.log("sin", { city: searchData.label, weatherResponse });
         setForecast({ city: searchData.label, ...forecastResponse });
       })
-      .catch(console.log("error en consola, es aqui??"));
+      .catch((err) => console.log(err));
   };
 
   console.log(forecast);
 
-  return (
+  return ( /* una vez que la promesa se resuelve se renderiza la app con todos los componentes integrados que conforman la aplicacion */
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
+      {currentWeather && <CurrentWeather data={currentWeather} />} { /* si se "rellene" currentweather y forecast se renderiza el componente <CurrentWea... y <Forecast */}
+      {forecast && <Forecast data={forecast} />} {/* y luego en current weather hago una destructuracion del objeto prop y saco la key "data" ---> ({data}) */}
       <CreatedBy />
     </div>
   );
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -10,25 +10,28 @@ const Search = ({ onSearchChange }) => {
         return fetch(
             `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
             geoApiOptions
-        )
-            .then((response) => response.json())
+        ) /* input value es lo que ingrese el user en el buscador */
+            .then((response) => response.json()) /* convertir un formato json a formato legible para js */
             .then((response) => {
+                console.log("response.data", response.data);
                 return {
                     options: response.data.map((city) => {
                         return {
                             value: `${city.latitude} ${city.longitude}`,
                             label: `${city.name}, ${city.countryCode}`,
                         };
-                    }),
+                    }),/* el map itera sobre un array y en cada indice del array retorna lo que le indiques usando "city" como una key para llamar a las propiedades que le interesa */
                 };
             });
     };
 
-    const handleOnChange = (searchData) => {
+    const handleOnChange = (searchData) => { /* pasand */
         setSearch(searchData);
         onSearchChange(searchData);
+        console.log("search data", {searchData});
     };
 
+/* en algun punto estas dos funciones conectan gracias a la libreria de asyncpaginate y hacen el traspaso de información // y hay el componente de asyncpagynate junta las dos funciones de loadoptions y handleonchange y hace una logica interna para enlazar la data */
     return (
         <AsyncPaginate
             placeholder="Search for city"
